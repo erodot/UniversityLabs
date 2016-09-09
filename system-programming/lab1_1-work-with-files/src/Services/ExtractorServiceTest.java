d4b280d11e411b31da9df30fbf17d1c1a67d09ae
erodot <theodoreromanus@gmail.com>
2016-09-10 01:29:07 +0300

    add new tests structures and comments

---
+++ system-programming/lab1_1-work-with-files/src/Services/ExtractorServiceTest.java
@@ -0,0 +1,13 @@
+package Services;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExtractorServiceTest {
+    @Test
+    public void extractWords() throws Exception {
+        // TODO: write tests for ExtractorService
+    }
+
+}
---
+++ system-programming/lab1_1-work-with-files/src/Services/IOServiceTest.java
@@ -0,0 +1,13 @@
+package Services;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IOServiceTest {
+    @Test
+    public void readFile() throws Exception {
+        // TODO: write tests for IOService
+    }
+
+}
