dbbc3385c5f681384835a798ac40cdc480fcafc5
Ted Romanus <theodoreromanus@gmail.com>
2017-05-10 04:25:21 +0300

    add table downloading

--- abit-site/index.js
+++ abit-site/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const request = require('requestretry');
 const cheerio = require('cheerio');
+const async = require('async');
 const $ = cheerio;
 
 const url = 'http://vstup.info';
@@ -10,8 +11,10 @@ const db = {
 };
 
 const DEBUG = false;
-const debug_number_of_universities = 100;
-const debug_number_of_tables = 1000;
+const debug_number_of_universities = 10;
+const debug_number_of_tables = 100;
+const maxAttempts = 25;
+const timeout = 5000;
 
 let schools_received = 0;
 let schools_sent = 0;
@@ -20,29 +23,6 @@ let region_sent = 0;
 let table_received = 0;
 let table_sent = 0;
 
-function parallelLimit(promiseFactories, limit) {
-  let result = [];
-  let cnt = 0;
-
-  function chain(promiseFactories) {
-    if(!promiseFactories.length) return;
-    let i = cnt++; // preserve order in result
-    return promiseFactories.shift().then((res) => {
-      result[i] = res; // save result
-      return chain(promiseFactories); // append next promise
-    });
-  }
-
-  let arrChains = [];
-  while(limit-- > 0 && promiseFactories.length > 0) {
-    // create `limit` chains which run in parallel
-    arrChains.push(chain(promiseFactories));
-  }
-
-  // return when all arrChains are finished
-  return Promise.all(arrChains).then(() => result);
-}
-
 const get_about_info = (cell) => {
     return $(cell).children().last().text()
 }
@@ -50,8 +30,8 @@ const get_about_info = (cell) => {
 const format_amount = (cell_object) => {
     let amount = $('nobr', cell_object);
     const amount_obj = {
-        'licensed': 0,
-        'budget': 0
+        licensed: 0,
+        budget: 0
     };
     if(amount.length > 0)
         amount_obj.licensed = eval($(amount[0]).text().replace("ЛО: ",""));
@@ -67,32 +47,31 @@ const format_contest_table_href = (cell_object) => {
     else return '';
 }
 
-const tablePromiseShellFor = (speciality) => (resolve, reject)=>{
-    if(speciality.contest_table_href.length == 0){
-        resolve();
-        return;
-    }
-    table_sent++;
-    request({url: url+speciality.contest_table_href, timeout: 5000}, function(error, response, body){
+const tablePromiseShellFor = (speciality, callback) => {
+    request({url: url+speciality.contest_table_href, timeout: timeout, maxAttempts: maxAttempts}, function(error, response, body){
         table_received++;
         if(error) {
-            reject(`Rejected ${table_received}/${table_sent}. ${url+speciality.contest_table_href} ${error}`);
+            callback(`Rejected ${table_received}/${table_sent}. ${url+speciality.contest_table_href} ${error}`);
             return;
         }
-        console.log(`Received: ${table_received}/${table_sent}...`);
-        resolve();
+        console.log(`Tables info receiving: ${table_received}/${table_sent}...`);
+        const $ = cheerio.load(body);
+        const data = $('.tablesaw-sortable tbody');
+        speciality.data = data.text();
+        if(table_received % 1000 == 0) global.gc();
+        callback(null);
     });
 };
 
 const schoolPromiseShellFor = (vnz) => (resolve, reject)=>{
     schools_sent++;
-    request({url:url + vnz.href, timeout: 5000}, function(error, response, body){
+    request({url:url + vnz.href, timeout: timeout, maxAttempts: maxAttempts}, function(error, response, body){
         schools_received++;
         if(error) {
             reject(`Rejected ${url+vnz.href}. ${error}`);
             return;
         }
-        console.log(`Schools info received: ${schools_received}/${schools_sent}...`);
+        console.log(`Schools info receiving: ${schools_received}/${schools_sent}...`);
 
         const $ = cheerio.load(body);
         const about_table = $('#about tr');
@@ -103,9 +82,9 @@ const schoolPromiseShellFor = (vnz) => (resolve, reject)=>{
         vnz.website = get_about_info(about_table[5]);
         vnz.email = get_about_info(about_table[6]).replace(" [ at ] ", "@");
         vnz.specialties = {
-            'full_time': { 'bachelors': [], 'masters': [], 'specialists': [], 'junior_specialists': [] },
-            'part_time': { 'bachelors': [], 'masters': [], 'specialists': [], 'junior_specialists': [] },
-            'distance_learning': { 'bachelors': [], 'masters': [], 'specialists': [], 'junior_specialists': [] }
+            full_time: { bachelors: [], masters: [], specialists: [], junior_specialists: [] },
+            part_time: { bachelors: [], masters: [], specialists: [], junior_specialists: [] },
+            distance_learning: { bachelors: [], masters: [], specialists: [], junior_specialists: [] }
         };
 
         const contest_tables_promise = (input_datas => {
@@ -114,14 +93,14 @@ const schoolPromiseShellFor = (vnz) => (resolve, reject)=>{
                 $(`#${input_data[0]} tbody tr`).each((index, elem)=>{
                     const data = $(elem).children(); //td's
                     const speciality = {
-                        'name': $(data[0]).text(),
-                        'contest_scores': $(data[1]).text(),
-                        'contest_table_href': format_contest_table_href(data[1]),
-                        'amount': format_amount(data[2]),
-                        'subjects': $(data[3]).text()
+                        name: $(data[0]).text(),
+                        contest_scores: $(data[1]).text(),
+                        contest_table_href: format_contest_table_href(data[1]),
+                        amount: format_amount(data[2]),
+                        subjects: $(data[3]).text()
                     };
                     input_data[1].push(speciality);
-                    contest_tables_promise.push(tablePromiseShellFor(speciality));
+                    contest_tables_promise.push(speciality);
                 });
             }));
             return contest_tables_promise;
@@ -130,14 +109,14 @@ const schoolPromiseShellFor = (vnz) => (resolve, reject)=>{
             ['zaoch1', vnz.specialties.part_time.bachelors], ['zaoch2', vnz.specialties.part_time.masters], ['zaoch3', vnz.specialties.part_time.specialists], ['zaoch4', vnz.specialties.part_time.junior_specialists],
             ['distan1', vnz.specialties.distance_learning.bachelors], ['distan2', vnz.specialties.distance_learning.masters], ['distan3', vnz.specialties.distance_learning.specialists], ['distan4', vnz.specialties.distance_learning.junior_specialists]
         ]);
-
+        if(schools_received % 100 == 0) global.gc(); 
         resolve(contest_tables_promise);
     });
     };
 
 const regionPromiseShellFor = (region) => (resolve, reject) => {
     region_sent++;
-    request(url + region.href, function(error, response, body){
+    request({url:url + region.href, maxAttempts: maxAttempts}, function(error, response, body){
         region_received++;
         if(error) {
             reject(error);
@@ -152,8 +131,8 @@ const regionPromiseShellFor = (region) => (resolve, reject) => {
                 $(`#${selector} td a`).each((index, elem)=>{
                     const data = $(elem);
                     const vnz = {
-                        'name': data.text(),
-                        'href': '/' + year + data.attr('href').slice(1).replace("#vnz", "")
+                        name: data.text(),
+                        href: '/' + year + data.attr('href').slice(1).replace("#vnz", "")
                     }
                     region.vnz.push(vnz);
                     school_promises.push(schoolPromiseShellFor(vnz));
@@ -175,8 +154,8 @@ request(url, (error, response, body)=>{
     $('#abet td a').each((index, elem) => {
         const data = $(elem);
         const region = {
-            'name': data.text(),
-            'href': data.attr('href').replace("#reg","")
+            name: data.text(),
+            href: data.attr('href').replace("#reg","")
         }
         db.regions.push(region);
         region_promises.push(regionPromiseShellFor(region));
@@ -188,19 +167,23 @@ request(url, (error, response, body)=>{
             console.log(`Received info about ${region_received} regions. Downloading universities info...`)
             const school_promises_global = [];
             school_promises_array.forEach(school_promises => school_promises.forEach(school_promise => school_promises_global.push(school_promise)));
-            if(DEBUG) school_promises_global.splice(debug_number_of_universities);
+            if(DEBUG && debug_number_of_universities > 0) school_promises_global.splice(debug_number_of_universities);
             Promise.all(school_promises_global.map(sp => new Promise(sp)))
                 .then(resolve)
                 .catch(reject);
         }))
         .then(contest_tables_promises_array => new Promise((resolve, reject) => {
             console.log(`Received info about ${schools_received} schools. Downloading rating lists...`);
-            const contest_tables_promises_global = [];
+            let contest_tables_promises_global = [];
             contest_tables_promises_array.forEach(contest_tables_promises => contest_tables_promises.forEach(contest_tables_promise => contest_tables_promises_global.push(contest_tables_promise)));
-            if(DEBUG) contest_tables_promises_global.splice(debug_number_of_tables);
-            Promise.all(contest_tables_promises_global.map(sp => new Promise(sp)))
-                .then(resolve)
-                .catch(reject);
+            contest_tables_promises_global = contest_tables_promises_global.filter(sp => sp.contest_table_href.length != 0);
+            if(DEBUG && debug_number_of_tables > 0) contest_tables_promises_global.splice(debug_number_of_tables);
+            table_sent = contest_tables_promises_global.length;
+            async.forEachLimit(contest_tables_promises_global, 100, tablePromiseShellFor, (error) => {
+                // all requests are done
+                if(error) reject(error);
+                else resolve();
+            })
         }))
         .then(() => new Promise((resolve,reject)=>{
             fs.writeFile('db.json', JSON.stringify( db, null, 4), function(err) {
