a1120085773b6bb3e7fc73ad391698335bd9bac1
Ted Romanus <tedromanus@Teds-MacBook-Pro.local>
2017-11-05 16:02:43 +0200

    it: first springboot application

---
+++ Information Systems/tdbms_springboot/src/test/java/com/knu/it/spring/TdbmsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.knu.it.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TdbmsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
